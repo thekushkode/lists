ab8feda54b2300ef5b99131933ab0ade125312be
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-06 11:04:24 -0400

    exercise2 from lists

---
+++ lists.2.py
@@ -0,0 +1,33 @@
+# List Exercise 2
+# convert infinite grocery item propt
+# only accept 3 items
+
+groc = []
+
+
+while len(groc) < 3:
+    needs = input('Enter one grocery item you need: ')
+    groc.append(needs)
+
+groc2 = []
+
+while len(groc2) < 3:
+    needs2 = input('Enter one grocery item you need: ')
+    groc2.append(needs2)
+
+
+groc_fin = groc + groc2
+#or groc_fin = groc.extend(groc2)
+print(groc_fin)
+
+#ask for index to be replaced
+index_to_replace = int(input('Enter the index of the iem you want to replace: '))
+
+#ask for item to add
+item_to_add = input('What item would you like to add: ')
+
+#adds item to final list based on user inputs
+groc_fin[index_to_replace] = item_to_add
+
+#prints new list
+print(groc_fin)
