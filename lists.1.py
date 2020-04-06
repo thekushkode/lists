737e9a9d365e0d56b4cfda702738026dabbbd0cc
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-06 10:07:18 -0400

    adding first list repo and folder

---
+++ lists.1.py
@@ -0,0 +1,19 @@
+#List exercise 1
+#Prompt user for single item and append it to list
+
+#do same thing in loop
+
+groc = ['milk', 'bread', 'beer']
+
+# need = input('Enter one grocery item: ')
+
+# groc.append(need)
+
+# print(groc)
+
+# While Loop Version
+
+while True:
+    needs = input('Enter one grocery item you need: ')
+    groc.append(needs)
+    print(groc)
