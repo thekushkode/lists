fe1c51a4512c75a157c5899a91c49dc457fd115b
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-06 20:36:43 -0400

    adding lists.py 2 & 3 with clean up edits

--- lists.2.py
+++ lists.2.py
@@ -35,3 +35,25 @@ groc_fin[index_to_replace] = item_to_add
 
 #prints new list
 print(groc_fin)
+
+#code w/o comments
+# groc = []
+
+# while len(groc) < 3:
+#     needs = input('Enter one grocery item you need: ')
+#     groc.append(needs)
+
+# groc2 = []
+
+# while len(groc2) < 3:
+#     needs2 = input('Enter one grocery item you need: ')
+#     groc2.append(needs2)
+
+# groc_fin = groc + groc2
+# print(groc_fin)
+
+# index_to_replace = int(input('Enter the index of the iem you want to replace: '))
+# item_to_add = input('What item would you like to add: ')
+# groc_fin[index_to_replace] = item_to_add
+
+# print(groc_fin)
